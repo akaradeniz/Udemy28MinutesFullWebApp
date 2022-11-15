4d149f08a9a1cfe6dbf744fd55d6d4f9a1d3eb88
Adile Karadeniz <adilek.qa@gmail.com>
2022-11-14 22:31:40 -0600

    First commend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule,  Routes} from "@angular/router";
+import { FormsModule} from "@angular/forms";
+import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes=[
+  {path:'',component:LoginComponent},
+  {path:'login',component: LoginComponent},
+  {path:'welcome/:name',component:WelcomeComponent},
+  {path:'listTodos',component:ListTodosComponent},
+  {path:'**',component:ErrorComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    ErrorComponent,
+    ListTodosComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/interfaces/ITodo.ts
@@ -0,0 +1,6 @@
+export interface ITodo{
+  id:number,
+  description: string,
+  tdate: Date,
+  done: boolean
+}
