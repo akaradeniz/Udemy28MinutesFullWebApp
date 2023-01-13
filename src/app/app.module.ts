5e3f09df7eee081e9288f71e56557ec0b53fda58
Adile Karadeniz <adilek.qa@gmail.com>
2023-01-12 22:22:43 -0600

    transfer my comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from "@angular/forms";
+
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule,  Routes} from "@angular/router";
-import { FormsModule} from "@angular/forms";
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { MenuComponent } from './menu/menu.component';
